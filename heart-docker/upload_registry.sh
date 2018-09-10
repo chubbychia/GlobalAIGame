0b7c665e99b8438fca66cf208665eb07db70e26e
Joey Wu <joey_wu@trend.com.tw>
2018-09-10 17:37:22 +0800

    If it's expired authentication during image upload, ask for username/pswd

--- heart-docker/upload_registry.sh
+++ heart-docker/upload_registry.sh
@@ -8,5 +8,14 @@ docker tag trend-hearts ai.registry.trendmicro.com/$project_name/trend-hearts:ra
 docker tag trend-hearts ai.registry.trendmicro.com/$project_name/trend-hearts:practice
 
 docker login ai.registry.trendmicro.com
+rc=$?
+if [[ $rc != 0 ]]; then
+    echo "Please enter your AD username to login trendmicro registry"
+    read username
+    echo "Please enter your AD password to login trendmicro registry"
+    read -s -p "Password:" pswd
+    docker login -u $username -p $pswd ai.registry.trendmicro.com
+fi
+
 docker push ai.registry.trendmicro.com/$project_name/trend-hearts:rank
 docker push ai.registry.trendmicro.com/$project_name/trend-hearts:practice
